81a06c21839e15e9ee9e480dd7380cf3859f24cd
anna-cardoso <annakaroline2021@usp.br>
2023-01-06 15:59:47 -0300

    add

--- src/routes/PublicRoutes/index.tsx
+++ src/routes/PublicRoutes/index.tsx
@@ -1,9 +1,17 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Cadastro from '@screens/Cadastro';
 import Home from '@screens/Home';
+import LandingPage from '@screens/LandingPage';
+import Login from '@screens/Login';
+import Perfil from '@screens/Perfil';
 import React from 'react';
 
 export type RootStackParamlist = {
+  Login: undefined;
   Home: undefined;
+  Cadastro: undefined;
+  Perfil: undefined;
+  LandingPage: undefined;
 };
 
 const { Navigator, Screen } = createNativeStackNavigator<RootStackParamlist>();
@@ -11,7 +19,11 @@ const { Navigator, Screen } = createNativeStackNavigator<RootStackParamlist>();
 const PublicRoutes = () => {
   return (
     <Navigator>
+      <Screen name="Login" component={Login} />
       <Screen name="Home" component={Home} />
+      <Screen name="Cadastro" component={Cadastro} />
+      <Screen name="Perfil" component={Perfil} />
+      <Screen name="LandingPage" component={LandingPage} />
     </Navigator>
   );
 };
